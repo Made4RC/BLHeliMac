550a70e3aa27fb8c4759f79c108a4135a6d49aff
Mac-D3 <norbert.kolb@web.de>
2016-02-06 21:43:25 +0100

    Create main.m

---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BLHeliMac
+//
+//  Created by Kolb Norbert on 18.01.16.
+//  Copyright © 2016 Kolb Norbert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
