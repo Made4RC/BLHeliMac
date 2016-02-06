efd98a9226d53918df7e83339213c5b9638e2c5b
Mac-D3 <norbert.kolb@web.de>
2016-02-06 21:44:31 +0100

    Create AppDelegate.m

---
+++ AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  BLHeliMac
+//
+//  Created by Kolb Norbert on 18.01.16.
+//  Copyright © 2016 Kolb Norbert. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
