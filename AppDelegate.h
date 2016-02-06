e72c24d301f2dc8cd3666d7e53b8de6e7fefad33
Mac-D3 <norbert.kolb@web.de>
2016-02-06 21:44:56 +0100

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLHeliMac
+//
+//  Created by Kolb Norbert on 18.01.16.
+//  Copyright © 2016 Kolb Norbert. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
